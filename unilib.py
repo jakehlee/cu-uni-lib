f3ecf0414b8958e57d8fa389ddf2566b988a01aa
Jake Lee <jakehlee1@gmail.com>
2018-09-09 02:32:58 -0400

    Successful web-scraping
    Read directory results and extract table text
    Chrome is running on headless mode
    Output seems well-structured, should be easy to convert to dict

--- unilib.py
+++ unilib.py
@@ -5,10 +5,17 @@ import csv
 import numpy as np
 from selenium import webdriver
 from selenium.webdriver.common.keys	import Keys
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--window-size=1920x1080")
+
 
 class AffilInfo:
 	""" Objects to store affiliate information """
 	def __init__(self):
+		self.valid = False
 		self.uni = ""
 		self.tel = ""
 		self.c_tel = ""
@@ -23,13 +30,43 @@ class AffilInfo:
 		self.email = ""
 
 def get_affil_info(uni):
-	driver = webdriver.Firefox()
+	affiliate = AffilInfo()
+	affiliate.uni = uni
+
+	driver = webdriver.Chrome(chrome_options=chrome_options)
 	driver.get("https://directory.columbia.edu/people/uni?code="+uni)
 	# 
 	assert "Columbia University: Directory" == driver.title
 
+	result_div = driver.find_elements_by_xpath("//div[@class='table_results_indiv']/table/tbody/tr/td")
+	print(len(result_div))
+
+	# clean up output
+	clean_output = []
+	for a_td in result_div:
+		if a_td.text != '' and a_td.text != ' ':
+			clean_output.append(a_td.text)
+	print(len(clean_output))
+	print(clean_output)
+
+	if len(clean_output) == 0:
+		# invalid UNI
+		return affiliate
+	else:
+		# valid UNI
+		affiliate.valid = True
+	return affiliate
+	#result_div = driver.find_elements_by_css_selector('div.table_results_indiv tr')
+	#for a_row in result_div:
+	#	print(a_row.text)
+
 def test_get_affil_info():
 	test_obj = get_affil_info('lcb50')
+	assert test_obj.uni == 'lcb50'
+	assert test_obj.valid == True
+	test_obj2 = get_affil_info('aaa11')
+	assert test_obj2.uni == 'aaa11'
+	assert test_obj2.valid == False
 
 def is_valid_uni(uni):
 	""" Validate the syntax of a uni string """
@@ -67,3 +104,4 @@ def test_is_valid_uni():
 
 if __name__ == "__main__":
 	test_is_valid_uni()
+	test_get_affil_info()
